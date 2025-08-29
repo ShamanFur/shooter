e95606e7b4bcec8659a82884c07d2e62c3dde7fe
Shaman <netunogames2@gmail.com>
2025-08-28 21:40:30 -0300

    loop da main criada conectando as classes de menu e game

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-import pygame
+from code.game import Game
 
-print('Sutup start')
-pygame.init()
-window = pygame.display.set_mode(size=(800, 600))
-print('Setup end')
-
-print('Start loop')
-while True:
-    #checando todos os eventos
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()#fechando janela
-            quit()#fechando o pygame
+game = Game()
+game.run()
