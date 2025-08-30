403a2fba77938d046d1d642ecdf237ed709f1f7a
Shaman <netunogames2@gmail.com>
2025-08-30 00:31:54 -0300

    alterando sprites

--- code/EntityFactory.py
+++ code/EntityFactory.py
@@ -12,9 +12,17 @@ class EntityFactory:
         match entity_name:
             case 'Level1bg':
                 list_bg = []
-                for i in range(4):
+                for i in range(3):
                     list_bg.append(Background(f'Level1bg{i}', (0, 0)))
                     list_bg.append(Background(f'Level1bg{i}', (WIN_WIDTH, 0)))
+
+                bg3_left = Background('Level1bg3', (0, WIN_HEIGHT))
+                bg3_left.rect.bottom = WIN_HEIGHT
+                list_bg.append(bg3_left)
+
+                bg3_right = Background('Level1bg3', (WIN_WIDTH, WIN_HEIGHT))
+                bg3_right.rect.bottom = WIN_HEIGHT
+                list_bg.append(bg3_right)
                 return list_bg
             case 'player':
                 return Player('player', (150, 470))
